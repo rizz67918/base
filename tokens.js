32746a6fc142a9e27c9c052ca915fd91107fad5f
rizz67918 <frilenwa@gmail.com>
2026-03-30 21:23:58 +0800

    Update tokens

--- tokens.js
+++ tokens.js
@@ -2,6 +2,7 @@
   "tokens": [
     "8158050976:AAGuWX2Aaex-ixwjFMw6nZt3OSui7x2M5cY",
     "8730554608:AAHZGq2RBXSBa0jC7dDW4uBwKrej2CUl5Ms",
-    " 8673300053:AAEFIM461JUt21ithhSbnQI8-ucqn-ftO9M"
+    " 8673300053:AAEFIM461JUt21ithhSbnQI8-ucqn-ftO9M",
+    "8735580586:AAHLgBowofiQnSv8jqhJhkMnzrlXIaSQb5o"
   ]
 }
